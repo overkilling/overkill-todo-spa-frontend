1e15e778e639fdad1d835d6c6239e87cd5c733d5
Paulo Schneider <paulo.schneider@gmail.com>
2020-05-30 10:11:22 +0100

    Add cypress test for homepage

--- cypress/integration/sample.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    cy.visit('https://example.cypress.io')
-
-    cy.contains('type').click()
-
-    cy.url().should('include', '/commands/actions')
-  })
-})
