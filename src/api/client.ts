f105c8c5d4d7d48a69b3c087b4b57f7581edffdc
Paulo Schneider <paulo.schneider@gmail.com>
2020-05-31 15:24:18 +0100

    Generate Todo types from schema

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,20 +1,24 @@
-import axios, { AxiosInstance } from 'axios'
-
-interface Todo {
-  todo: string
-}
+import Ajv from 'ajv'
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios'
+import todoSchema from './todos.schema.json'
+import { Todo } from './types'
 
 export class TodoClient {
   private httpClient: AxiosInstance
+  private validateTodoResponse: ResponseDecorator<Todo[]>
 
   constructor(apiBaseUrl: string) {
     this.httpClient = axios.create({
       baseURL: apiBaseUrl
     })
+    this.validateTodoResponse = schemaValidator<Todo[]>(todoSchema)
   }
 
   async getTodos(): Promise<Todo[]> {
-    return this.httpClient.get<Todo[]>('/todos').then(async ({ data }) => data)
+    return this.httpClient
+      .get('/todos')
+      .then(this.validateTodoResponse)
+      .then(({ data }) => data)
   }
 
   async isApiHealthy(): Promise<boolean> {
@@ -22,5 +26,20 @@ export class TodoClient {
       .get('/health')
       .then(({ data: { status } }) => status === 'ok')
       .catch(() => false)
+  }
+}
+
+type ResponseDecorator<T> = (response: AxiosResponse<T>) => AxiosResponse<T>
+
+const schemaValidator = <T>(schema: object): ResponseDecorator<T> => {
+  const validate = Ajv().compile(schema)
+
+  return (response: AxiosResponse<T>) => {
+    if (!validate(response.data)) {
+      const error = new Error('invalid response schema') as AxiosError
+      error.response = response
+      throw error
+    }
+    return response
   }
 }
